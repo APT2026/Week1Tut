c8510caceea833fa75f8d89d6601a73ef79cb130
sandipdhakal2024 <sandip.dhakal@icp.edu.np>
2026-02-20 13:07:11 +0545

    ADD: added topic related logic

--- src/main/java/com/ingcollegeapt/week1tut/Week1Tut.java
+++ src/main/java/com/ingcollegeapt/week1tut/Week1Tut.java
@@ -1,22 +1,45 @@
 package com.ingcollegeapt.week1tut;
 
+import com.ingcollegeapt.week1tut.model.Topic;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Week1Tut {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final List<Topic> topics = new ArrayList<>();
 
     public static void main(String[] args) {
         while (true) {
             showMenu();
             String choice = scanner.next();
+            scanner.nextLine();
 
             switch (choice) {
                 case "1":
                     // TODO: Add topic
+                    System.out.print("Enter the topic name: ");  
+                    String topicName = scanner.nextLine();
+                    if(topicName.isEmpty()){
+                        System.out.println("Topic not added!");
+                        break;
+                    } 
+                    int id = topics.size()+1;   // id will start from 1 so size +1
+                    Topic topic = new Topic(id, topicName);  // create topic object to add to arraylist
+                    topics.add(topic);                       // add to the topics list  
+                    System.out.println("Topic is added successfully!");
                     break;
                 case "2":
                     // TODO: View topics
+                    if(topics.isEmpty()){
+                        System.out.println("Np Topics available");
+                        break;
+                    } 
+                    for (Topic topi: topics){
+                        System.out.println("ID: "+topi.getTopicId()+" Name: "+topi.getTopicName()
+                        +" created Date:"+ topi.getCreatedAt().toLocalDate());
+                    }                  
                     break;
                 case "3":
                     // TODO: Add entry to a topic
--- src/main/java/com/ingcollegeapt/week1tut/model/Topic.java
+++ src/main/java/com/ingcollegeapt/week1tut/model/Topic.java
@@ -1,9 +1,54 @@
 package com.ingcollegeapt.week1tut.model;
 
 //Create a POJO Class for Topic
+
+import java.time.LocalDateTime;
+
 /*
 It has id, name, created and updated TimeStamp
 */
 public class Topic {
+    private int topicId;
+    private String topicName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Topic(int topicId, String topicName) {
+        this.topicId = topicId;
+        this.topicName = topicName;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getTopicId() {
+        return topicId;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setTopicId(int topicId) {
+        this.topicId = topicId;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    
+    
+    
+    
     
 }
